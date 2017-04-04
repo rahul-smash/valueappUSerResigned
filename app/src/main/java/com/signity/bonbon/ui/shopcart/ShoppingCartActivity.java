96cb27d2154702a5fbc732e5e8d2eaeaebbb653d
Pardeep <rajinder.k@signitysolutions.in>
2017-04-04 15:41:37 +0530

    Common Code intermediate by Pardeep

--- app/src/main/java/com/signity/bonbon/ui/shopcart/ShoppingCartActivity.java
+++ app/src/main/java/com/signity/bonbon/ui/shopcart/ShoppingCartActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;

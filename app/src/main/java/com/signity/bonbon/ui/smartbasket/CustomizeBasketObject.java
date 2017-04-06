e13bafcf707135e2b6b1059a0919811a6780079f
Pardeep <rajinder.k@signitysolutions.in>
2017-04-06 14:35:56 +0530

    Common Code internationalization intermediate by Pardeep

--- app/src/main/java/com/signity/bonbon/ui/smartbasket/CustomizeBasketObject.java
+++
@@ -1,9 +0,0 @@
-package com.signity.bonbon.ui.smartbasket;
-
-/**
- * Created by root on 2/9/15.
- */
-public class CustomizeBasketObject {
-    public String text,items_name,items_price;
-    public int images,items,count=0;
-}
